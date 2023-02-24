4542c615b675aea9471b12ade7714f2656bab082
Jcastel2014 <2020253542@ub.edu.bz>
2023-02-23 22:17:56 -0600

    Start question 3

---
+++ q3/go.mod
@@ -0,0 +1,3 @@
+module github.com/Jcastel2014/q3
+
+go 1.19
---
+++ q3/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func processOne() {
+	fmt.Println("Process 1 completed")
+
+}
+
+func processTwo() {
+	fmt.Println("Process 2 completed")
+
+}
+func main() {
+	processOne()
+	processTwo()
+}
