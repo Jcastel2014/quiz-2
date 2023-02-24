7f406e100422f6224c20edf00afb17751b736660
Jcastel2014 <2020253542@ub.edu.bz>
2023-02-23 22:49:44 -0600

    finish question 2

--- q2/main.go
+++ q2/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func trueToFalse(channel chan bool) {
+
+	channel <- true
+
+	fmt.Println("Message Sent")
+
+}
+
+func check(channel chan bool) {
+	for !<-channel {
+		fmt.Println("Awaiting Message")
+	}
+
+	fmt.Println("Message Recieved")
+
+}
 func main() {
+
+	channel := make(chan bool, 1)
+
+	channel <- false
+
+	go check(channel)
+	go trueToFalse(channel)
+
+	time.Sleep(100 * time.Millisecond)
 
 }
